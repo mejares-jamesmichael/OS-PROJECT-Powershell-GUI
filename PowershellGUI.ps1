6ff94ee02ab2f3de6a7d3fcf24300c1ba6837ab8
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2024-12-05 01:17:18 +0800

    Add info on Dev Panel

--- PowershellGUI.ps1
+++ PowershellGUI.ps1
@@ -17,23 +17,30 @@ $Panel.Height = 50
 
 #Panel Text in the Homepage
 $PanelText=New-Object $LabelObject # Sets the label
-$PanelText.Text='♜ [O/S Project✔] ➣ 穏やかな.exe                                                 ⍰ ❏ ✖' # Text
+$PanelText.Text='♜ [O/S Project✔] ➣ 穏やかな.exe                                             ⍰   ❏   ✖' # Text
 $PanelText.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#FFFCF5") #Font color
 $PanelText.AutoSize= $true
 $PanelText.Font='Impact, 13, style=Bold'
 $PanelText.Location=New-Object System.Drawing.Point(5,15) # x, y
 
+$HeaderText=New-Object $LabelObject # Sets the label
+$HeaderText.Text='C:\Windows\system32\powershellGUI.ps1' # Text
+$HeaderText.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000") #Font color
+$HeaderText.AutoSize= $true
+$HeaderText.Font='Consolas,11, style=Underline'
+$HeaderText.Location=New-Object System.Drawing.Point(10,55) # x, y
+
 #Panel Page for Quick Guide Button
 $Panel2 = New-Object System.Windows.Forms.Panel
 $backgroundHexColor = "#FFFFFF" #hex color
 $Panel2.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
-$Panel2.Size = New-Object System.Drawing.Size(255, 300)
+$Panel2.Size = New-Object System.Drawing.Size(255, 290)
 $Panel2.Location = New-Object System.Drawing.Point(30, 90)
-$Panel2.Visible = $true
+$Panel2.Visible = $false
 
 #Panel Text in Quick Guide Button
 $PanelText2=New-Object $LabelObject # Sets the label
-$PanelText2.Text='Quick Guide Window  _ ❏ ✖ ' # Text
+$PanelText2.Text=' Quick Guide Window _ ❏ ✖ ' # Text
 $PanelText2.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#FFFCF5") #Font color
 $backgroundHexColor = "#073AA3" #hex color
 $PanelText2.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
@@ -48,31 +55,80 @@ $backgroundHexColor = "#FFFFFF" #hex color
 $PanelText3.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
 $PanelText3.AutoSize= $true
 $PanelText3.Font='Consolas, 10'
-$PanelText3.Location=New-Object System.Drawing.Point(10,120) # x, y
+$PanelText3.Location=New-Object System.Drawing.Point(10,118) # x, y
 
 $PanelText4=New-Object $LabelObject # Sets the label
-$PanelText4.Text='Check Disk Analyzer ➜➜➜➜➜' # Text
+$PanelText4.Text='Check DiskAnalyzer           ➜' # Text
 $PanelText4.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000") #Font color
 $backgroundHexColor = "#FFFFFF" #hex color
 $PanelText4.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
 $PanelText4.AutoSize= $true
 $PanelText4.Font='Consolas, 10'
-$PanelText4.Location=New-Object System.Drawing.Point(10,160) # x, y
+$PanelText4.Location=New-Object System.Drawing.Point(10,155) # x, y
 
 #Panel Page for Learn More about Devs
 $Panel3 = New-Object System.Windows.Forms.Panel
-$backgroundHexColor = "#FFFFFF" #hex color
+$backgroundHexColor = "#B3B4BA" #hex color grey
 $Panel3.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
-$Panel3.Size = New-Object System.Drawing.Size(440, 300)
+$Panel3.Size = New-Object System.Drawing.Size(440, 290)
 $Panel3.Location = New-Object System.Drawing.Point(30, 90)
-$Panel3.Visible = $false
+$Panel3.Visible = $true
 
-$HeaderText=New-Object $LabelObject # Sets the label
-$HeaderText.Text='Need Help?' # Text
-$HeaderText.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000") #Font color
-$HeaderText.AutoSize= $true
-$HeaderText.Font='Consolas,11 , style=Underline'
-$HeaderText.Location=New-Object System.Drawing.Point(10,55) # x, y
+#Panel Page for Learn More about Devs
+$Panel4 = New-Object System.Windows.Forms.Panel
+$backgroundHexColor = "#FFFFFF" #hex color white
+$Panel4.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
+$Panel4.Size = New-Object System.Drawing.Size(400, 200)
+$Panel4.Location = New-Object System.Drawing.Point(20, 70)
+$Panel4.Visible = $true
+
+$PanelText5=New-Object $LabelObject # Sets the label
+$PanelText5.Text=' DevNote Window ver1.01                 _ ❏ ✖ ' # Text
+$PanelText5.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#FFFCF5") #Font color
+$backgroundHexColor = "#073AA3" #hex color blue
+$PanelText5.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
+$PanelText5.AutoSize= $true
+$PanelText5.Font='Consolas, 12, style=Bold'
+$PanelText5.Location=New-Object System.Drawing.Point(0,0) # x, y
+
+$PanelText6=New-Object $LabelObject # Sets the label
+$PanelText6.Text='C:\Windows\system32\powershellGUI.ps1\ReadDevNote.txt' # Text
+$PanelText6.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000") #Font color
+$backgroundHexColor = "#B3B4BA" #hex color
+$PanelText6.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
+$PanelText6.AutoSize= $true
+$PanelText6.Font='Consolas, 10,style=Underline'
+$PanelText6.Location=New-Object System.Drawing.Point(10,30) # x, y
+
+$PanelText7=New-Object $LabelObject # Sets the label
+$PanelText7.Text='DEV(s)_ n Contributors➭' # Text
+$PanelText7.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000") #Font color
+$backgroundHexColor = "#FFFFFF" #hex color
+$PanelText7.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
+$PanelText7.AutoSize= $false
+$PanelText7.Font='Consolas, 20, style=Bold'
+$PanelText7.Size = New-Object System.Drawing.Size(250, 80) # Adjust size for multi-line text
+$PanelText7.Location=New-Object System.Drawing.Point(10,10) # x, y
+
+$PanelText8=New-Object $LabelObject # Sets the label
+$PanelText8.Text='JAMES MICHAEL MEJARES    ########### ### ###### #### ######## ####' # Text
+$PanelText8.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000") #Font color
+$backgroundHexColor = "#FFFFFF" #hex color
+$PanelText8.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
+$PanelText8.AutoSize= $false
+$PanelText8.Font='Lucida Console, 12'
+$PanelText8.Size = New-Object System.Drawing.Size(230, 80) # Adjust size for multi-line text
+$PanelText8.Location=New-Object System.Drawing.Point(50,90) # x, y
+
+$MenuButton5=New-Object $ButtonObject # CheckTheDevs Button
+$MenuButton5.Text='Rate Us! ☻'
+$MenuButton5.AutoSize= $true
+$MenuButton5.Font='Lucida Console, 11, style=Regular'
+$MenuButton5.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#000000")
+$backgroundHexColor = "#CACBCD" #hex color
+$MenuButton5.BackColor = [System.Drawing.ColorTranslator]::FromHtml($backgroundHexColor) #translating hex color
+$MenuButton5.Size=New-Object System.Drawing.Size(10,10)
+$MenuButton5.Location=New-Object System.Drawing.Point(280,150)
 
 #HomnePage Window
 $MenuWindow=New-Object $FormObject 
@@ -182,6 +238,8 @@ $MenuButton4.Add_Click({ #Triggers when Learn More about devs is clicked
 $MenuWindow.Controls.AddRange(@( $Panel, $Panel2, $Panel3, $HeaderText, $MenuText, $MenuText2, $MenuText3, $MenuButton, $MenuButton2, $MenuButton3, $MenuButton4))
 $Panel.Controls.Add($PanelText)
 $Panel2.Controls.AddRange(@($PanelText2, $PanelText3, $PanelText4))
+$Panel3.Controls.AddRange(@($Panel4, $PanelText5, $PanelText6))
+$Panel4.Controls.AddRange(@($PanelText7,$PanelText8, $MenuButton5))
 
 # Form 2 - Automated Disk Cleanup
 $AutoDiskWindow = New-Object $FormObject
